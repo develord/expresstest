cc2f15a0fdf806f2b3bc9d07317a150d1b9e5d7b
aminefr <mohamed.amine.fredj@gmail.com>
2021-07-12 14:10:46 +0100

    add log

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
-//Allow all requests from all domains & localhost
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "POST, GET")
-  next()
-})
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 
 const oraganigram = {
@@ -88,7 +78,7 @@ const oraganigram = {
         ]
       }
     ]
-  }
+}
 
 
 app.get('/oraganigram', function(req, res) {
